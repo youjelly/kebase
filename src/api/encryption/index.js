b56a829843893a7ff78b2dd6be787595e9754812
youjelly <itsusama@gmail.com>
2021-10-27 12:34:34 -0700

    stub

---
+++ src/api/encryption/index.js
@@ -0,0 +1,37 @@
+import fs from 'fs';
+
+class EncryptionService {
+  constructor() {
+
+  }
+
+  async encrypt(filePath, key) {
+    
+    return true;
+  }
+
+  async decrypt(filePath, privateKey) {
+    return true;
+  }
+
+  async generateSharedKey() {
+    return true;
+  }
+
+  async generateFileHeader(sharedKey, publikKeys, filePath) {
+    return true;
+  }
+
+  async checkDefaultFoldersForKeypair() {
+    if (process.platform === 'win32') {
+      if (fs.existsSync(`${process.env.HOMEPATH}/.ssh/id_rsa`)) 
+        return {privateKeyPath: `${process.env.HOMEPATH}/.ssh/id_rsa`}
+      return {privateKeyPath: ``}
+    }
+    if (fs.existsSync(`~/.ssh/id_rsa`)) 
+        return {privateKeyPath: `~/.ssh/id_rsa`}
+      return {privateKeyPath: ``}
+  }
+}
+
+export default EncryptionService;
